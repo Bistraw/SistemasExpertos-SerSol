086a44c02c1823a3d80ff55180a74692de9e0427
MurasakiWolf <143193084+MurasakiWolf@users.noreply.github.com>
2026-02-24 21:27:47 -0600

    Catalogo de carreras del centro de ciencias basicas

--- utils.py
+++ utils.py
@@ -18,3 +18,17 @@ def print_fact(fact):
     print(f"{key}: {fact[key]}", end=", ")
 
   print(" }")
+
+CARRERAS = {
+  1: ("Ing. Bioquímico", 398),
+  2: ("Ing. en Computación Inteligente", 400),
+  3: ("Ing. en Electrónica", 367),
+  4: ("Ing. en Sistemas Computacionales", 394),
+  5: ("Ing. Industrial Estadístico", 371),
+  6: ("Lic. en Biología", 383),
+  7: ("Lic. en Biotecnología", 391),
+  8: ("Lic. en Desarrollo de Videojuegos y Entornos Virtuales", 360),
+  9: ("Lic. en Informática y Tecnologías Computacionales", 390),
+  10: ("Lic. en Matemáticas Aplicadas", 351),
+  11: ("Químico Farmacéutico Biólogo", 400)
+}
